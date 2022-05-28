2f5b725522780179ba09e1556efdc880cb0e75ce
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-28 15:13:16 +0000

    add4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
+
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def update
+  def edit
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to  user_path(@user.id)
