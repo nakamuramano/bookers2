a0ac9eb86286722a06865265dc710dc15967bbce
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-29 06:19:07 +0000

    add4-25

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     devise_for :users
     root to: "homes#top"
     get "homes/about" => "homes#about"
-    resources :books, only: [:new,:index,:show,:edit,:create,]
+    resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
     resources :users, only: [:index,:show,:edit,]
 
 end
