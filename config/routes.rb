09448fdc218f02bb710eb1cf70d108698a5af682
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-29 12:49:09 +0000

    add4-25

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get "homes/about" => "homes#about"
     resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
-    resources :users, only: [:index,:show,:edit,]
+    resources :users, only: [:index,:show,:edit, :update]
 
 end
