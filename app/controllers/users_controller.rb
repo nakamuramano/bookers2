e906d997f78552302bc1c4318e07450c74ffe9b4
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-31 13:41:20 +0000

    add4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :correct_user, only: [:edit]
+
   def index
     @users = User.all
     @user = current_user
@@ -32,6 +34,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user.id) unless @user == current_user
   end
 
 
