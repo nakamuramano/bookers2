8ae77a51a7c546d2659b33d3be9d454f2cb5a2b3
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-06-02 13:00:29 +0000

    finish4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -58,7 +58,7 @@ class BooksController < ApplicationController
   def correct_user
     @book = Book.find(params[:id])
     @user = @book.user
-    redirect_to books_path(current_user.id) unless @user == current_user
+    redirect_to books_path unless @user == current_user
   end
 
 
